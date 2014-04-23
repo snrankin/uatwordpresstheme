eb30b2831eb8d8caa41e9c501564c4dc6acc7a8e
Sam <snrankin@me.com>
2014-04-23 16:41:18 -0700

    Updated entry style

--- entry-footer.php
+++ entry-footer.php
@@ -1,4 +1,9 @@
 <footer class="entry-footer">
+<section class="entry-meta">
+<span class="author vcard"><?php the_author_posts_link(); ?></span>
+<span class="meta-sep"> | </span>
+<?php edit_post_link(); ?>
+</section>
 <span class="cat-links"><?php _e( 'Categories: ', 'blankslate' ); ?><?php the_category( ', ' ); ?></span>
 <span class="tag-links"><?php the_tags(); ?></span>
 <?php if ( comments_open() ) { 
--- entry.php
+++ entry.php
@@ -1,8 +1,8 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header>
-<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?> <?php edit_post_link(); ?>
-<?php if ( !is_search() ) get_template_part( 'entry', 'meta' ); ?>
+<?php if( !is_singular() ) { echo ('<div class="entry-date"><i class="fa fa-calendar"></i>'); the_time( get_option( 'date_format' ) ); echo ('</div>');} ?>
+<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
 </header>
-<?php get_template_part( 'entry', ( is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
+<?php get_template_part( 'entry', ( is_archive() || is_search() || is_home() ? 'summary' : 'content' ) ); ?>
 <?php if ( !is_search() ) get_template_part( 'entry-footer' ); ?>
 </article>
