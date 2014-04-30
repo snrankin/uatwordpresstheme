d699bea2431b50b313fbabd661b94302c88d017f
Sam Rankin <snrankin@me.com>
2014-04-30 15:01:23 -0700

    More style changes

--- entry-meta.php
+++ entry-meta.php
@@ -1,5 +1,5 @@
 <section class="entry-meta">
-<span class="author vcard"><?php the_author_posts_link(); ?></span>
+<span class="author vcard"><i class="fa fa-user"></i> <?php the_author_posts_link(); ?></span>
 <span class="meta-sep"> | </span>
 <span class="entry-date"><?php the_time( get_option( 'date_format' ) ); ?></span>
 </section>
