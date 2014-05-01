bbe894326d1be3319378c5d85257233407cde16c
Sam Rankin <snrankin@me.com>
2014-05-01 10:36:07 -0700

    Set up footer

--- footer.php
+++ footer.php
@@ -2,8 +2,22 @@
 </div>
 <footer id="footer" role="contentinfo">
 <div class="inner-wrap">
-<div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?> <?php wp_loginout(); ?>
+<div id="address" class="footer-widget">
+	<h3>Location</h3>
+    <p>2625 W. Baseline Rd.<br/>Tempe, AZ, 85283<br/>
+    <i class="fa fa-map-marker"></i> map</p>
+</div>
+<div id="contact" class="footer-widget">
+<h3>Contact</h3>
+<p><i class="fa fa-phone"></i> 800-658-5744</p>
+<p><i class="fa fa-envelope"></i> webmaster@uat.edu</p>
+</div>
+<div id="social" class="footer-widget">
+<h3>Follow UAT</h3>
+<p><i class="fa fa-facebook-square"></i> Facebook<br/>
+<i class="fa fa-twitter-square"></i> Twitter<br/>
+<i class="fa fa-google-plus-square"></i> Google+<br/>
+</p>
 </div>
 </div>
 </footer>
