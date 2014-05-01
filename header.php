1aff33132174da2ff67fa2ef60d9acf718e0ead7
Sam Rankin <snrankin@me.com>
2014-05-01 09:52:43 -0700

    Added UAT logo

--- header.php
+++ header.php
@@ -21,6 +21,7 @@
 <header id="header" role="banner">
 <div class="inner-wrap">
 <section id="branding">
+<div id="site-logo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home"><img src="<?php echo get_template_directory_uri(); ?>/imgs/UATlogo.png" /></a></div>
 <div id="site-title"><h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php esc_attr_e( get_bloginfo( 'name' ), 'blankslate' ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a></h1></div>
 <div id="site-description"><?php bloginfo( 'description' ); ?></div>
 </section>
