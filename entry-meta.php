fb733fef16cc9c82c1351674f0095f2a1ac116a9
Sam Rankin <snrankin@me.com>
2014-05-05 11:05:49 -0700

    Added author page

--- entry-meta.php
+++ entry-meta.php
@@ -2,7 +2,7 @@
 <h3><i class="fa fa-calendar"></i> Posted On:</h3>
 <span class="entry-date-single"><?php the_time( get_option( 'date_format' ) ); ?></span>
 <h3><i class="fa fa-folder"></i> Catagories</h3>
-<span class="cat-links"<?php the_category( ', ' ); ?></span>
+<span class="cat-links"><?php the_category( ', ' ); ?></span>
 <span class="tag-links"><?php
 if( has_tag() ) {
 echo '<h3><i class="fa fa-tags"></i> Tags</h3>'; the_tags();
--- entry.php
+++ entry.php
@@ -1,7 +1,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <header>
 <?php if( !is_singular() ) { echo ('<div class="entry-date"><i class="fa fa-calendar"></i> '); the_time( get_option( 'date_format' ) ); echo ('</div>');} ?>
-<?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
+<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
 </header>
 <?php get_template_part( 'entry', ( is_archive() || is_search() || is_home() ? 'summary' : 'content' ) ); ?>
 <?php if ( !is_singular() ) get_template_part( 'entry-footer' ); ?>
