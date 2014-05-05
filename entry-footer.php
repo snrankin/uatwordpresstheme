080eb10d9ac1b3fbd2f12f459e6a88dd1df7efc3
Sam Rankin <snrankin@me.com>
2014-05-05 09:32:14 -0700

    Minor changes - Removed relative position from .entry-date (might need a fix for chrome) - removed php tag for sharing plugin from single sidebar

--- entry-footer.php
+++ entry-footer.php
@@ -1,19 +1,18 @@
 <footer class="entry-footer">
 <section class="entry-meta">
 <span class="author vcard"><i class="fa fa-user"></i> <?php the_author_posts_link(); ?></span>
-<span class="meta-sep"> | </span>
 <?php edit_post_link(); ?>
 </section>
 <span class="cat-links"><i class="fa fa-folder"></i><?php the_category( ', ' ); ?></span>
 <span class="tag-links"><?php
 if( has_tag() ) {
-echo '<i class="fa fa-tags"></i>'; the_tags();
+echo '<i class="fa fa-tags"></i>'; the_tags(' ');
 }
 else {
 // IF NO TAGS
 }
 ?></span>
 <?php if ( comments_open() ) { 
-echo '<span class="meta-sep">|</span><i class="fa fa-comments"></i> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'blankslate' ) ) . '</a></span>';
+echo '<span class="meta-sep">|</span> <i class="fa fa-comments"></i> <span class="comments-link"><a href="' . get_comments_link() . '">' . sprintf( __( 'Comments', 'blankslate' ) ) . '</a></span>';
 } ?>
 </footer> 
--- sidebar-single.php
+++ sidebar-single.php
@@ -2,7 +2,6 @@
 	<div class="widget-container">
 	<?php get_template_part( 'entry' , 'meta' ); ?>
     </div>
-    <?php if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) { ADDTOANY_SHARE_SAVE_KIT(); } ?>
       <div class="widget-container">
   	  <?php display_related_posts_via_taxonomies(); ?>
       </div>
